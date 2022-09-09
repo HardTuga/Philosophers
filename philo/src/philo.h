c1ffcc90eb689ec2bf24014fdee75a21f71651b3
Pedro Campos martins <pcampos-@c2r10s5.42lisboa.com>
2022-09-09 17:03:14 +0100

    v0.1

--- philo/src/philo.h
+++ philo/src/philo.h
@@ -6,22 +6,35 @@
 /*   By: pcampos- <pcampos-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/06 14:38:12 by pcampos-          #+#    #+#             */
-/*   Updated: 2022/09/06 15:59:53 by pcampos-         ###   ########.fr       */
+/*   Updated: 2022/09/09 15:36:01 by pcampos-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_H
-# define LIBFT_H
+#ifndef PHILO_H
+# define PHILO_H
 
 //------------------------------INCLUDES------------------------------//
 # include	"../libft/libft.h"
-# include <sys/time.h>
+# include   <sys/time.h>
+# include	<pthread.h>
 
 //------------------------------STRUCTS------------------------------//
+typedef struct s_data
+{
+	int	n_philos;
+	int	die;
+	int	eat;
+	int	sleep;
+	int	repeat;
+}				t_data;
+
+typedef struct s_philo
+{
+	int	*dead;
+	pthread_t	philo_id;
+}				t_philo;
 
 //------------------------------PHILO_MAIN------------------------------//
-
-//------------------------------CHECK_ARGS------------------------------//
-int	check_args(int ac, char **av);
+int check_args(int ac, char **av);
 
 #endif
--- philo/src/philo_main.c
+++ philo/src/philo_main.c
@@ -1,22 +1,65 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   philo_main.c                                       :+:      :+:    :+:   */
+/*  philo_main.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: pcampos- <pcampos-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/06 12:19:10 by pcampos-          #+#    #+#             */
-/*   Updated: 2022/09/06 14:46:13 by pcampos-         ###   ########.fr       */
+/*   Updated: 2022/09/09 15:21:17 by pcampos-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
+int	check_args(int ac, char **av)
+{
+	int i;
+	int j;
+
+	i = -1;
+	if(ac < 4 || ac > 5)
+		return (0);
+	while(av[++i])
+	{
+		while(av[i][++j])
+		{
+			if(!ft_isdigit(av[i]))
+				return (0);
+		}
+		j = -1;
+	}
+	return (1);
+}
+
+int	fill_struct(char **av, t_data *data)
+{
+	data->n_philos = ft_atoi(av[1]);
+	data->die = ft_atoi(av[2]);
+	data->eat = ft_atoi(av[3]);
+	data->sleep = ft_atoi(av[4]);
+	if (av[5])
+		data->repeat = ft_atoi(av[5]);
+	if (data->n_philos < 1 || data->die < 1 || data->eat < 1 ||
+		data->sleep < 1 || data->repeat < 1)
+		return (0);
+	return (1);
+}
+
 int	main(int ac, char **av)
 {
+	t_data data;
+	t_philo	*philo;
+
 	if(!check_args(ac, av))
 	{
-		printf("Erro nos argumentos");
-		return (0);
+		ft_putstr_fd("Erro nos argumentos\n", 2);
+		return (-1);
 	}
+	if(!fill_struct(av, &data))
+	{
+		ft_putstr_fd("Erro no valor dos argumentos\n", 2);
+		return (-1);
+	}
+	philo = malloc(sizeof(t_philo) * data.n_philos);
 }
